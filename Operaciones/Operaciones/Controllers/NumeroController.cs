33b08046d92c683ab29500a73622a8840565448a
Oscar Nicolas Landivar Aguilera <a2019114151@estudiantes.upsa.edu.bo>
2020-12-12 11:52:15 -0400

    Programa funcionando

---
+++ Operaciones/Operaciones/Controllers/NumeroController.cs
@@ -0,0 +1,33 @@
+﻿using operaciones.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace operaciones.Controllers
+{
+    [RoutePrefix("api/operaciones")]
+    public class NumeroController : ApiController
+    {
+        [HttpGet]
+        [Route("{number:int}")]
+        public numeromodelo Operations(int number)
+        {
+            numeromodelo model = new numeromodelo();
+            model.numero = number;
+
+            if (number < 0)
+                model.mensaje = "ERROR";
+
+            if (number == 0)
+                model.mensaje = "Realizado por Oscar Landivar";
+
+            if (number > 0)
+                model.mensaje = "https://image.freepik.com/vector-gratis/numeros-cartel-imagen_1639-6453.jpg";
+
+            return model;
+        }
+    }
+}
---
+++ Operaciones/Operaciones/Models/numeromodelo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace operaciones.Models
+{
+    public class numeromodelo
+    {
+        [Key]
+        public int numero { get; set; }
+        public string mensaje { get; set; }
+    }
+}
